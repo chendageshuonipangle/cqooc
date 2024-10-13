629fe5cc01dc5e5aaf48ed032a7788b36b7a61a2
chen54 <2829612742@qq.com>
2024-10-13 15:48:53 +0800

    temp

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-10-13
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.cqooc.com/course/detail/courseStudy?id=334573650&kkzt=true
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
